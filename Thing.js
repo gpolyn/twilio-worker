4f852b9f4f21fc52a3fe5b19cc1a10e63b130045
Gallagher Polyn <gallagher.polyn@gmail.com>
2015-05-30 17:32:12 -0400

    removed Thing.js

--- Thing.js
+++
@@ -1,6 +0,0 @@
-// var mongoose = require('mongoose');
-// var AbstractThing = require('./AbstractThing').AbstractThing;
-
-// var Thing = new AbstractThing();
-
-// module.exports = mongoose.model('Thing', Thing);
